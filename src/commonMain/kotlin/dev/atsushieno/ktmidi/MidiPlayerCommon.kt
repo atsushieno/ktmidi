3f239dd51878db8126bf8a92a04b20adb152b395
atsushieno <atsushieno@gmail.com>
2021-04-11 03:01:50 +0900

    moved common EventLooper part to common file.

--- src/commonMain/kotlin/dev/atsushieno/ktmidi/MidiPlayerCommon.kt
+++ src/commonMain/kotlin/dev/atsushieno/ktmidi/MidiPlayerCommon.kt
@@ -4,7 +4,148 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.Runnable
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Semaphore
+import kotlin.jvm.Volatile
 
+internal abstract class MidiEventLooper<TMessage>(private val timer: MidiPlayerTimer) {
+    var starting: Runnable? = null
+    var finished: Runnable? = null
+    var playbackCompletedToEnd: Runnable? = null
+
+    private var doPause: Boolean = false
+    private var doStop: Boolean = false
+    var tempoRatio: Double = 1.0
+
+    var state: PlayerState
+    internal var eventIdx = 0
+    var currentTempo = MidiMetaType.DEFAULT_TEMPO
+    var currentTimeSignature = ByteArray(4)
+    var playDeltaTime: Int = 0
+
+    private var eventLoopSemaphore : Semaphore? = null
+    @Volatile
+    var clientNeedsSpinWait = false
+    @Volatile
+    var clientNeedsCloseSpinWait = false
+
+    init {
+        state = PlayerState.STOPPED
+    }
+
+    fun close() {
+        if(eventLoopSemaphore?.availablePermits == 0)
+            eventLoopSemaphore?.release()
+        if (state != PlayerState.STOPPED) {
+            clientNeedsCloseSpinWait = true
+            stop()
+            var dummyCloseCounter = 0
+            while (clientNeedsCloseSpinWait) {
+                if (dummyCloseCounter++ < 0) //  spinwait, or overflow
+                    break
+            }
+        }
+    }
+
+    fun play() {
+        state = PlayerState.PLAYING
+        eventLoopSemaphore?.release()
+    }
+
+    abstract fun mute()
+
+    fun pause() {
+        doPause = true
+        mute()
+    }
+
+    suspend fun playBlocking() {
+        try {
+            starting?.run()
+
+            eventIdx = 0
+            playDeltaTime = 0
+            eventLoopSemaphore = Semaphore(1, 0)
+            var doWait = true
+            while (true) {
+                if (doWait) {
+                    eventLoopSemaphore?.acquire()
+                    clientNeedsSpinWait = false
+                    doWait = false
+                }
+                if (doStop)
+                    break
+                if (doPause) {
+                    doWait = true
+                    doPause = false
+                    state = PlayerState.PAUSED
+                    continue
+                }
+                if (isEventIndexAtEnd())
+                    break
+                processMessage(getNextMessage())
+            }
+            doStop = false
+            state = PlayerState.STOPPED
+            mute()
+            if (isEventIndexAtEnd())
+                playbackCompletedToEnd?.run()
+            finished?.run()
+        } finally {
+            clientNeedsCloseSpinWait = false
+        }
+    }
+
+    abstract fun isEventIndexAtEnd() : Boolean
+    abstract fun getNextMessage() : TMessage
+    abstract fun getContextDeltaTimeInMilliseconds(m: TMessage): Int
+    abstract fun getDurationOfEvent(m: TMessage) : Int
+    abstract fun updateTempoAndTimeSignatureIfApplicable(m: TMessage)
+    abstract fun onEvent(m: TMessage)
+
+    private suspend fun processMessage(m: TMessage) {
+        if (seek_processor != null) {
+            val result = seek_processor!!.filterMessage(m)
+            when (result) {
+                SeekFilterResult.PASS_AND_TERMINATE,
+                SeekFilterResult.BLOCK_AND_TERMINATE ->
+                    seek_processor = null
+            }
+
+            when (result) {
+                SeekFilterResult.BLOCK,
+                SeekFilterResult.BLOCK_AND_TERMINATE ->
+                    return // ignore this event
+            }
+        } else {
+            val ms = getContextDeltaTimeInMilliseconds(m)
+            if (ms > 0) {
+                timer.waitBy(ms)
+                playDeltaTime += getDurationOfEvent(m)
+            }
+        }
+
+        updateTempoAndTimeSignatureIfApplicable(m)
+
+        onEvent(m)
+    }
+
+    fun stop() {
+        if (state != PlayerState.STOPPED)
+            doStop = true
+        if (eventLoopSemaphore?.availablePermits == 0)
+            eventLoopSemaphore?.release()
+    }
+
+    private var seek_processor: SeekProcessor<TMessage>? = null
+
+    // not sure about the interface, so make it non-public yet.
+    internal fun seek(seekProcessor: SeekProcessor<TMessage>, ticks: Int) {
+        seek_processor = seekProcessor
+        eventIdx = 0
+        playDeltaTime = ticks
+        mute()
+    }
+}
 
 abstract class MidiPlayerCommon<TMessage>(internal val output: MidiOutput, private var shouldDisposeOutput: Boolean, timer: MidiPlayerTimer) {
 
