4f508fc36b745b590ed9d4bfa5c708f7d74da04e
atsushieno <atsushieno@gmail.com>
2021-08-01 17:51:53 +0900

    RtMidiAccess: implement virtual ports.

--- ktmidi-jvm-desktop/src/main/kotlin/dev/atsushieno/ktmidi/RtMidiAccess.kt
+++ ktmidi-jvm-desktop/src/main/kotlin/dev/atsushieno/ktmidi/RtMidiAccess.kt
@@ -40,6 +40,19 @@ class RtMidiAccess() : MidiAccess() {
 
     // Input/Output
 
+    override val canCreateVirtualPort: Boolean
+        get() = when(library.rtmidi_out_get_current_api(library.rtmidi_out_create_default())) {
+            RtmidiLibrary.RtMidiApi.RTMIDI_API_LINUX_ALSA,
+            RtmidiLibrary.RtMidiApi.RTMIDI_API_MACOSX_CORE -> true
+            else -> false
+        }
+
+    override suspend fun createVirtualInputSender(context: PortCreatorContext) =
+        RtMidiVirtualOutput(context)
+
+    override suspend fun createVirtualOutputReceiver(context: PortCreatorContext) =
+        RtMidiVirtualInput(context)
+
     override suspend fun openInputAsync(portId: String): MidiInput =
         RtMidiInput(portId.toInt())
 
@@ -47,51 +60,135 @@ class RtMidiAccess() : MidiAccess() {
         RtMidiOutput(portId.toInt())
 
     abstract class RtMidiPort : MidiPort {
-        override val connectionState = MidiPortConnectionState.OPEN // at created state
         override var midiProtocol: Int = 0 // unspecified
         abstract override val details: MidiPortDetails
         abstract override fun close()
     }
 
-    class RtMidiInput(private val portIndex: Int) : MidiInput, RtMidiPort() {
-        private val rtmidi = library.rtmidi_in_create_default()
-
-        override val details: MidiPortDetails
-            get() = RtMidiPortDetails(portIndex, library.rtmidi_get_port_name(rtmidi, portIndex))
-
-        override fun close() {
-            library.rtmidi_close_port(rtmidi)
-        }
-
+    private class RtMidiInputHandler(private val rtmidi: RtMidiWrapper) {
         private var listener: OnMidiReceivedEventListener? = null
 
-        override fun setMessageReceivedListener(listener: OnMidiReceivedEventListener) {
+        fun setMessageReceivedListener(listener: OnMidiReceivedEventListener) {
             this.listener = listener
         }
 
-        private fun onRtMidiMessage(timestamp: Double, message: Pointer, messageSize: NativeSize, userData: Pointer) {
+        private fun onRtMidiMessage(timestamp: Double, message: Pointer, messageSize: NativeSize) {
             listener?.onEventReceived(message.getByteArray(0, messageSize.toInt()), 0, messageSize.toInt(), (timestamp * 1_000_000_000).toLong())
         }
 
         init {
             library.rtmidi_in_set_callback(rtmidi,
-                { timestamp, message, messageSize, userData -> onRtMidiMessage(timestamp, message, messageSize, userData) },
+                { timestamp, message, messageSize, _ -> onRtMidiMessage(timestamp, message, messageSize) },
                 Pointer.NULL)
         }
     }
 
-    class RtMidiOutput(private val portIndex: Int) : MidiOutput, RtMidiPort() {
-        private val rtmidi = library.rtmidi_out_create_default()
+    class RtMidiInput(private val portIndex: Int) : MidiInput, RtMidiPort() {
+        private val rtmidi = library.rtmidi_in_create_default()
+        override var connectionState = MidiPortConnectionState.OPEN // at created state
+        private val inputHandler : RtMidiInputHandler = RtMidiInputHandler(rtmidi)
 
         override val details: MidiPortDetails
             get() = RtMidiPortDetails(portIndex, library.rtmidi_get_port_name(rtmidi, portIndex))
 
         override fun close() {
+            connectionState = MidiPortConnectionState.CLOSED
+            library.rtmidi_close_port(rtmidi)
+        }
+
+        override fun setMessageReceivedListener(listener: OnMidiReceivedEventListener) {
+            inputHandler.setMessageReceivedListener(listener)
+        }
+
+        init {
+            library.rtmidi_open_port(rtmidi, portIndex, "ktmidi port $portIndex")
+        }
+    }
+
+    class RtMidiOutput(private val portIndex: Int) : MidiOutput, RtMidiPort() {
+        private val rtmidi = library.rtmidi_out_create_default()
+        override var connectionState = MidiPortConnectionState.OPEN // at created state
+
+        override val details: MidiPortDetails
+            get() = RtMidiPortDetails(portIndex, library.rtmidi_get_port_name(rtmidi, portIndex))
+
+        override fun close() {
+            connectionState = MidiPortConnectionState.CLOSED
             library.rtmidi_close_port(rtmidi)
         }
 
         override fun send(mevent: ByteArray, offset: Int, length: Int, timestamp: Long) {
             library.rtmidi_out_send_message(rtmidi, if (offset > 0) mevent.drop(offset).take(length).toByteArray() else mevent, length)
+        }
+
+        init {
+            library.rtmidi_open_port(rtmidi, portIndex, "ktmidi port $portIndex")
+        }
+    }
+
+    // Virtual ports
+
+    class RtMidiVirtualPortDetails(context: PortCreatorContext) : MidiPortDetails {
+        override val id: String
+        override val manufacturer: String
+        override val name: String
+        override val version: String
+
+        init {
+            id = context.portName
+            name = context.portName
+            manufacturer = context.manufacturer
+            version = context.version
+        }
+    }
+
+    abstract class RtMidiVirtualPort(context: PortCreatorContext) : MidiPort {
+        private val detailsImpl: MidiPortDetails
+
+        override val details: MidiPortDetails
+            get() = detailsImpl
+
+        override var midiProtocol: Int = 0
+
+        init {
+            detailsImpl = RtMidiVirtualPortDetails(context)
+        }
+    }
+
+    class RtMidiVirtualInput(context: PortCreatorContext) : MidiInput, RtMidiVirtualPort(context) {
+        private val rtmidi = library.rtmidi_in_create_default()
+        override var connectionState = MidiPortConnectionState.OPEN // at created state
+        private val inputHandler : RtMidiInputHandler = RtMidiInputHandler(rtmidi)
+
+        override fun close() {
+            connectionState = MidiPortConnectionState.CLOSED
+            library.rtmidi_close_port(rtmidi)
+        }
+
+        override fun setMessageReceivedListener(listener: OnMidiReceivedEventListener) {
+            inputHandler.setMessageReceivedListener(listener)
+        }
+
+        init {
+            library.rtmidi_open_virtual_port(rtmidi, context.portName)
+        }
+    }
+
+    class RtMidiVirtualOutput(context: PortCreatorContext) : MidiOutput, RtMidiVirtualPort(context) {
+        private val rtmidi = library.rtmidi_out_create_default()
+        override var connectionState = MidiPortConnectionState.OPEN // at created state
+
+        override fun close() {
+            connectionState = MidiPortConnectionState.CLOSED
+            library.rtmidi_close_port(rtmidi)
+        }
+
+        override fun send(mevent: ByteArray, offset: Int, length: Int, timestamp: Long) {
+            library.rtmidi_out_send_message(rtmidi, if (offset > 0) mevent.drop(offset).take(length).toByteArray() else mevent, length)
+        }
+
+        init {
+            library.rtmidi_open_virtual_port(rtmidi, context.portName)
         }
     }
 }
