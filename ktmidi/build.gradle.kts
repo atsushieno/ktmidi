88d31d602e93376928ea436a7dad16a41bd35099
atsushieno <atsushieno@gmail.com>
2021-10-14 03:40:27 +0900

    ktmidi module: migrate to build.gradle.kts.

---
+++ ktmidi/build.gradle.kts
@@ -0,0 +1,218 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithTests
+
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("me.tylerbwong.gradle:metalava-gradle:0.1.9")
+    }
+}
+
+plugins {
+    id("com.android.library") version "4.1.3"
+    kotlin("multiplatform") version "1.5.21"
+    id("org.jetbrains.dokka") version "1.5.0"
+    id("maven-publish")
+    id("signing")
+    id("me.tylerbwong.gradle.metalava") version "0.1.9"
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnit()
+        }
+    }
+    android {
+        publishLibraryVariantsGroupedByFlavor = true
+        publishLibraryVariants("debug", "release")
+    }
+    js(BOTH) {
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+        }
+        nodejs {
+        }
+    }
+    /*
+    iosX64("ios") {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    */
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val isMacOSX64 = hostOs == "Mac OS X"
+    val isLinuxX64 = hostOs == "Linux"
+    val nativeTarget = when {
+        isMacOSX64 -> macosX64("apple") {
+            binaries {
+                staticLib {}
+                sharedLib {}
+            }
+        }
+        isLinuxX64 -> linuxX64("linuxX64") {
+            binaries {
+                staticLib {}
+                sharedLib {}
+            }
+        }
+        isMingwX64 -> mingwX64("mingwX64") {
+            binaries {
+                staticLib {}
+                sharedLib {}
+            }
+        }
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+    
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
+                implementation("io.ktor:ktor-io:1.6.0")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting {
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.core:core-ktx:1.6.0")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("junit:junit:4.13.2")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+        val jsMain by getting {
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+        val nativeMain by creating {
+        }
+        val nativeTest by creating {
+        }
+        /*
+        val iosMain by getting {
+        }
+        val iosTest by getting {
+        }
+        */
+    }
+}
+
+metalava {
+    filename = "api/$name-api.txt"
+    outputKotlinNulls = false
+    includeSignatureVersion = false
+}
+
+android {
+    compileSdkVersion(31)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["test"].assets.srcDir("src/commonTest/resources") // kind of hack...
+    defaultConfig {
+        minSdkVersion(24)
+        targetSdkVersion(31)
+    }
+    buildTypes {
+        val debug by getting {
+            minifyEnabled(false)
+        }
+        val release by getting {
+            minifyEnabled(false)
+        }
+    }
+}
+
+afterEvaluate {
+
+    val javadocJar by tasks.registering(Jar::class) {
+        archiveClassifier.set("javadoc")
+    }
+
+    publishing {
+        publications.withType<MavenPublication> {
+            artifact(javadocJar)
+            pom {
+                name.set("ktmidi")
+                description.set("Kotlin Multiplatform library for MIDI 1.0 and MIDI 2.0")
+                url.set("https://github.com/atsushieno/ktmidi")
+                scm {
+                    url.set("https://github.com/atsushieno/ktmidi")
+                }
+                licenses {
+                    license {
+                        name.set("the MIT License")
+                        url.set("https://github.com/atsushieno/ktmidi/blob/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("atsushieno")
+                        name.set("Atsushi Eno")
+                        email.set("atsushieno@gmail.com")
+                    }
+                }
+            }
+        }
+
+        repositories {
+            /*
+                maven {
+                    name = "GitHubPackages"
+                    url = uri("https://maven.pkg.github.com/atsushieno/ktmidi")
+                    credentials {
+                        username = project.findProperty("gpr.user") ?: System.getenv("USERNAME")
+                        password = project.findProperty("gpr.key") ?: System.getenv("GITHUB_TOKEN")
+                    }
+                }
+                */
+            maven {
+                name = "OSSRH"
+                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = System.getenv("OSSRH_USERNAME")
+                    password = System.getenv("OSSRH_PASSWORD")
+                }
+            }
+        }
+    }
+
+    // keep it as is. It is replaced by CI release builds
+    signing {}
+}
