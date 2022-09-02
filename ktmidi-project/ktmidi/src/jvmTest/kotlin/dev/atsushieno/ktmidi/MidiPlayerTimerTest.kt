0fa434061f05c834b7891cc646fd8367c2aee6f6
atsushieno <atsushieno@gmail.com>
2022-09-02 10:12:09 +0800

    MidiPlayerTimerTest still seems to give intermittent failures, so extend timeout

--- ktmidi-project/ktmidi/src/jvmTest/kotlin/dev/atsushieno/ktmidi/MidiPlayerTimerTest.kt
+++ ktmidi-project/ktmidi/src/jvmTest/kotlin/dev/atsushieno/ktmidi/MidiPlayerTimerTest.kt
@@ -15,10 +15,10 @@ class MidiPlayerTimerTest {
             val start = timeSource.markNow()
             val timer = SimpleAdjustingMidiPlayerTimer(timeSource)
             for (i in 0 .. 4) {
-                timer.waitBySeconds(0.4)
+                timer.waitBySeconds(1.0)
                 val delta = start.elapsedNow().inWholeMilliseconds
-                assertTrue(delta >= 400.0 * (i + 1), "wait never happened: $delta msec. at $i")
-                assertTrue(delta < 400.0 * (i + 2), "wait taking too much time: $delta msec. at $i")
+                assertTrue(delta >= 1000.0 * (i + 1), "wait never happened: $delta msec. at $i")
+                assertTrue(delta < 1000.0 * (i + 2), "wait taking too much time: $delta msec. at $i")
             }
         }
     }
