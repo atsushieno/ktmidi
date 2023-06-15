c2d573ec05ef073dc146a93595f1ccb312a0854c
Atsushi Eno <atsushieno@gmail.com>
2023-06-15 17:42:44 +0900

    modernize native builds: they cross-compile now.

--- ktmidi/build.gradle.kts
+++ ktmidi/build.gradle.kts
@@ -44,21 +44,16 @@ kotlin {
         }
     }
 
-    iosArm64 { binaries { framework { baseName = "library" } } }
-    iosX64 { binaries { framework { baseName = "library" } } }
-    iosSimulatorArm64 { binaries {framework { baseName = "library" } } }
+    iosArm64 { binaries { framework { baseName = "ktmidi" } } }
+    iosX64 { binaries { framework { baseName = "ktmidi" } } }
+    iosSimulatorArm64 { binaries {framework { baseName = "ktmidi" } } }
 
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+    macosArm64()
+    macosX64()
+    // kotlinx-datetime is not built enough to make it possible...
+    //linuxArm64()
+    linuxX64()
+    mingwX64()
 
     sourceSets {
         val commonMain by getting {
@@ -103,8 +98,14 @@ kotlin {
                 implementation(kotlin("test-js"))
             }
         }
-        val nativeMain by getting
-        val nativeTest by getting
+        val nativeMain by creating
+        val nativeTest by creating
+        val macosArm64Main by getting
+        val macosX64Main by getting
+        // kotlinx-datetime is not built enough to make it possible...
+        //val linuxArm64Main by getting
+        val linuxX64Main by getting
+        val mingwX64Main by getting
         val iosArm64Main by getting
         val iosArm64Test by getting
         val iosSimulatorArm64Main by getting
