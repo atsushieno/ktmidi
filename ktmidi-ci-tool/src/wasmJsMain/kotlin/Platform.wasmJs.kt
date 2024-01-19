b9125498105dc7321dbb058b9cae52a7daf7dbca
Atsushi Eno <atsushieno@gmail.com>
2024-01-19 18:06:57 +0900

    [citool] wasm browser version now saves the settings (simple to LocalStorage)

--- ktmidi-ci-tool/src/wasmJsMain/kotlin/Platform.wasmJs.kt
+++ ktmidi-ci-tool/src/wasmJsMain/kotlin/Platform.wasmJs.kt
@@ -1,12 +1,14 @@
 import androidx.compose.runtime.Composable
+import dev.atsushieno.ktmidi.toUtf8ByteArray
+import kotlinx.browser.window
 
 class WasmPlatform: Platform {
     override val name: String = "Web with Kotlin/Wasm"
     override val canReadLocalFile = false
     override fun loadFileContent(path: String): ByteArray =
-        byteArrayOf()// TODO("FIXME: implement")
+        window.localStorage.getItem(path)?.toUtf8ByteArray() ?: byteArrayOf()
     override fun saveFileContent(path: String, bytes: ByteArray) =
-        TODO("FIXME: implement")
+        window.localStorage.setItem(path, bytes.decodeToString())
 
     @Composable
     override fun BinaryFilePicker(show: Boolean, fileChosen: (String?) -> Unit) =
