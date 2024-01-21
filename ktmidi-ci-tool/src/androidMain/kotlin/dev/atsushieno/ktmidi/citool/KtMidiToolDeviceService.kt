7a0165826097cf685bedcadfb83df29e267be662
Atsushi Eno <atsushieno@gmail.com>
2024-01-21 17:54:59 +0900

    [citool] fix MidiReceiver field initialization order.
    On MidiDeviceService, super.onCreate() has to be called *at last*.

--- ktmidi-ci-tool/src/androidMain/kotlin/dev/atsushieno/ktmidi/citool/KtMidiToolDeviceService.kt
+++ ktmidi-ci-tool/src/androidMain/kotlin/dev/atsushieno/ktmidi/citool/KtMidiToolDeviceService.kt
@@ -1,18 +1,27 @@
 package dev.atsushieno.ktmidi.citool
 
+import AndroidPlatform
 import android.media.midi.MidiDeviceService
 import android.media.midi.MidiReceiver
 import android.media.midi.MidiSender
+import androidPlatform
+import dev.atsushieno.ktmidi.ci.MidiCIDevice
 
 class KtMidiToolDeviceService : MidiDeviceService() {
+    private lateinit var device: MidiCIDevice
     private lateinit var receiver: KtMidiToolReceiver
     private lateinit var sender: KtMidiToolSender
 
     override fun onCreate() {
-        super.onCreate()
+        androidPlatform = androidPlatform ?: AndroidPlatform(applicationContext)
         initializeAppModel(applicationContext)
         receiver = KtMidiToolReceiver()
         sender = KtMidiToolSender()
+        device = MidiCIDevice()
+        // It is usually in the wrong order, but MidiDeviceService invokes onGetInputPortReceivers(),
+        // which returns the lateinit var that is being initialized *in this method*.
+        // So, it must be invoked later than initialization.
+        super.onCreate()
     }
     override fun onGetInputPortReceivers(): Array<MidiReceiver> {
         return arrayOf(receiver)
