7de5d3db30ddb69fbc4f408ce782beb8eaff87d9
Atsushi Eno <atsushieno@gmail.com>
2024-01-23 23:32:56 +0900

    midi-ci: cosmetic source file refactoring

--- ktmidi-ci/src/commonMain/kotlin/dev/atsushieno/ktmidi/ci/MidiCIConstants.kt
+++ ktmidi-ci/src/commonMain/kotlin/dev/atsushieno/ktmidi/ci/MidiCIConstants.kt
@@ -164,19 +164,3 @@ object MidiCIConstants {
     const val STANDARD_DEFINED_PROFILE: Byte = 0x7E
 }
 
-object MidiCIConverter {
-
-    fun encodeStringToASCII(s: String): String {
-        return if (s.all { it.code < 0x80 && !it.isISOControl() })
-            s
-        else
-            s.map { if (it.code < 0x80) it.toString() else "\\u${it.code.toString(16)}" }.joinToString("")
-    }
-    fun decodeASCIIToString(s: String): String =
-        s.split("\\u").mapIndexed { index, e ->
-            if (index == 0)
-                e
-            else
-                e.substring(0, 4).toInt(16).toChar() + s.substring(4)
-        }.joinToString("")
-}
