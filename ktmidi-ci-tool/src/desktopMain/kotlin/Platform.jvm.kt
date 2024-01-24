1f016fada3aa07c4a111806168cbc30b37e88f8a
Atsushi Eno <atsushieno@gmail.com>
2024-01-25 00:10:41 +0900

    citool: on desktop, save the config file under user config directory.

--- ktmidi-ci-tool/src/desktopMain/kotlin/Platform.jvm.kt
+++ ktmidi-ci-tool/src/desktopMain/kotlin/Platform.jvm.kt
@@ -2,15 +2,31 @@ import androidx.compose.runtime.Composable
 import com.darkrockstudios.libraries.mpfilepicker.FilePicker
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.io.path.exists
 
 class JVMPlatform : Platform {
     override val name: String = "Java ${System.getProperty("java.version")}"
     override val canReadLocalFile = true
+
+    private val toolName = "ktmidi-ci-tool"
     override fun loadFileContent(path: String): ByteArray =
-        Files.readAllBytes(Path.of(path))
+        Files.readAllBytes(Path.of(ensureAppConfigPath(), toolName, path))
 
     override fun saveFileContent(path: String, bytes: ByteArray) {
-        Files.write(Path.of(path), bytes)
+        Files.write(Path.of(ensureAppConfigPath(), toolName, path), bytes)
+    }
+
+    private fun ensureAppConfigPath(): String {
+        val os = System.getProperty("os.name")
+        val ret = when {
+            os.startsWith("windows", true) -> System.getenv("LOCALAPPDATA")
+            os.startsWith("mac", true) -> System.getProperty("user.home") + "/Library/Application Support"
+            else -> System.getProperty("user.home") + "/.config"
+        }
+        val toolDir = Path.of(ret, toolName)
+        if (!toolDir.exists())
+            Files.createDirectory(toolDir)
+        return ret
     }
 
     @Composable
