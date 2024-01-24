9a88a38bffac3d7a3419f8b5865a33dea143a5c5
Atsushi Eno <atsushieno@gmail.com>
2024-01-25 03:26:46 +0900

    midi-ci: fix versionId in DeviceInfo resource. It only contained 3 bytes.

--- ktmidi-ci/src/commonMain/kotlin/dev/atsushieno/ktmidi/ci/MidiCIDeviceInfo.kt
+++ ktmidi-ci/src/commonMain/kotlin/dev/atsushieno/ktmidi/ci/MidiCIDeviceInfo.kt
@@ -15,17 +15,23 @@ data class MidiCIDeviceInfo(
     var serialNumber: String? = null
 ) {
 
-    private fun toBytes(v: Int) = listOf(
+    private fun toBytes3(v: Int) = listOf(
         (v and 0x7F).toByte(),
         ((v shr 8) and 0x7F).toByte(),
         ((v shr 16) and 0x7F).toByte()
+    )
+    private fun toBytes4(v: Int) = listOf(
+        (v and 0x7F).toByte(),
+        ((v shr 8) and 0x7F).toByte(),
+        ((v shr 16) and 0x7F).toByte(),
+        ((v shr 24) and 0x7F).toByte()
     )
     private fun toBytes(v: Short) = listOf(
         (v.toInt() and 0x7F).toByte(),
         ((v.toInt() shr 8) and 0x7F).toByte()
     )
-    fun manufacturerIdBytes() = toBytes(manufacturerId)
+    fun manufacturerIdBytes() = toBytes3(manufacturerId)
     fun familyIdBytes() = toBytes(familyId)
     fun modelIdBytes() = toBytes(modelId)
-    fun versionIdBytes() = toBytes(versionId)
+    fun versionIdBytes() = toBytes4(versionId)
 }
