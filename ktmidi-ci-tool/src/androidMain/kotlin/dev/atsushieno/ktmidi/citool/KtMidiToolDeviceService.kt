6abf9e8029a9791e78ed31cb63404b559bac91b3
Atsushi Eno <atsushieno@gmail.com>
2024-01-25 18:11:28 +0900

    citool: Android virtual MIDI out port was implemented in wrong way.

--- ktmidi-ci-tool/src/androidMain/kotlin/dev/atsushieno/ktmidi/citool/KtMidiToolDeviceService.kt
+++ ktmidi-ci-tool/src/androidMain/kotlin/dev/atsushieno/ktmidi/citool/KtMidiToolDeviceService.kt
@@ -10,13 +10,16 @@ import dev.atsushieno.ktmidi.ci.MidiCIDevice
 class KtMidiToolDeviceService : MidiDeviceService() {
     private lateinit var repository: CIToolRepository
     private lateinit var receiver: KtMidiToolReceiver
-    private lateinit var sender: KtMidiToolSender
 
     override fun onCreate() {
         androidPlatform = androidPlatform ?: AndroidPlatform(applicationContext)
         repository = CIToolRepository()
         receiver = KtMidiToolReceiver(repository)
-        sender = KtMidiToolSender(repository)
+
+        repository.midiDeviceManager.midiOutputSent.add { bytes, timestamp ->
+            outputPortReceivers.forEach { it.send(bytes, 0, bytes.size, timestamp) }
+        }
+
         // It is usually in the wrong order, but MidiDeviceService invokes onGetInputPortReceivers(),
         // which returns the lateinit var that is being initialized *in this method*.
         // So, it must be invoked later than initialization.
@@ -31,23 +34,5 @@ class KtMidiToolReceiver(private val repository: CIToolRepository) : MidiReceive
     override fun onSend(msg: ByteArray?, offset: Int, count: Int, timestamp: Long) {
         if (msg != null)
             repository.ciDeviceManager.processMidiInput(msg, offset, count)
-    }
-}
-
-class KtMidiToolSender(repository: CIToolRepository) : MidiSender() {
-    private val receivers = mutableListOf<MidiReceiver>()
-    override fun onConnect(receiver: MidiReceiver?) {
-        if (receiver != null)
-            receivers.add(receiver)
-    }
-
-    override fun onDisconnect(receiver: MidiReceiver?) {
-        receivers.remove(receiver)
-    }
-
-    init {
-        repository.midiDeviceManager.midiOutputSent.add { bytes, timestamp ->
-            receivers.forEach { it.send(bytes, 0, bytes.size, timestamp) }
-        }
     }
 }
