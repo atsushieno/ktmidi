48ad6942f163d9b9f3c648e375d33a03fd24914f
Atsushi Eno <atsushieno@gmail.com>
2024-04-13 04:21:47 +0900

    I dumped the idea of getting rid of bunch of extraneous repos on sonatype...

--- ktmidi-native-ext/build.gradle.kts
+++ ktmidi-native-ext/build.gradle.kts
@@ -123,7 +123,8 @@ afterEvaluate {
         repositories {
             maven {
                 name = "OSSRH"
-                url = uri("https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/$repositoryId/")
+                //url = uri("https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/$repositoryId/")
+                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
                 credentials {
                     username = System.getenv("OSSRH_USERNAME")
                     password = System.getenv("OSSRH_PASSWORD")
