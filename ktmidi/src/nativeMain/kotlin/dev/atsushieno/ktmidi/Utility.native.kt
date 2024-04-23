55488623ac77f5850455f1247f7853bcc810bb46
Atsushi Eno <atsushieno@gmail.com>
2024-04-23 11:35:23 +0900

    Get ready for Kotlin 2.0 syntax change.
    In Kotlin 2.0, when(enum) without `else` in expect/actual function is not
    regarded as exhaustive.

--- ktmidi/src/nativeMain/kotlin/dev/atsushieno/ktmidi/Utility.native.kt
+++ ktmidi/src/nativeMain/kotlin/dev/atsushieno/ktmidi/Utility.native.kt
@@ -7,4 +7,5 @@ internal actual fun stringToByteArray(s: String): ByteArray = s.toByteArray()
 internal actual fun nativeByteOrder(): ByteOrder = when(KtorByteOrder.nativeOrder()) {
     KtorByteOrder.BIG_ENDIAN -> ByteOrder.BIG_ENDIAN
     KtorByteOrder.LITTLE_ENDIAN -> ByteOrder.LITTLE_ENDIAN
+    else -> throw UnsupportedOperationException()
 }
