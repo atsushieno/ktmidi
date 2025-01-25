c24dc1acf9dd47c154a6531d2e36df01dcf02dfb
Atsushi Eno <atsushieno@gmail.com>
2025-01-25 10:58:13 +0900

    tentatively go back to Kotlin 2.0. 2.1 causes various metadata build failures.

--- ktmidi-ci-tool/build.gradle.kts
+++ ktmidi-ci-tool/build.gradle.kts
@@ -1,6 +1,5 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
@@ -14,7 +13,6 @@ plugins {
 }
 
 kotlin {
-    @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
     wasmJs {
         moduleName = "ktmidi-ci-tool"
         browser {
